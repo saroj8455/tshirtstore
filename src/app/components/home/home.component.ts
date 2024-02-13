ba8ca50d54684936756ee7713b4c00ab946ccaf9
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-02-13 23:36:02 +0530

    update sample form layout

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  cities!: City[];
+  selectedCity!: City;
 
+  selectedCities!: City[];
+
+  ngOnInit(): void {
+    this.loadTemplate();
+  }
+
+  // initial config
+  loadTemplate() {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' },
+    ];
+  }
 }
